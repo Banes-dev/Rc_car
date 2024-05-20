1f41d7f60b7169ae4ee29ad99a7d6b35aad1c4aa
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-05-20 03:04:48 +0200

    test with 180 value

--- servo.py
+++ servo.py
@@ -4,9 +4,9 @@ from time import sleep
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
-    servo.angle = 90
+    servo.angle = 180
     sleep(2)
     servo.angle = 0
     sleep(2)
-    servo.angle = -90
+    servo.angle = -180
     sleep(2)
