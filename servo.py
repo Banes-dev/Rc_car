b975349c6c2c4fb0399497d6ba201baa71f10469
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-05-20 03:31:45 +0200

    create main.py and add function

--- servo.py
+++ servo.py
@@ -1,9 +1,18 @@
 from gpiozero import AngularServo
 from time import sleep
 
+# Utils function
+def map_value(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
+    # function for get joystick nb
+    # if nb_joystick > 0
+        # transfor joystick nb with map function between -20 and 90
+    # if nb_joystick > 0
+        # transfor joystick nb with map function between -90 and 0
     servo.angle = -20
     sleep(0.2)
     servo.angle = 90
@@ -12,3 +21,7 @@ while (True):
     sleep(0.2)
     servo.angle = -90
     sleep(0.2)
+
+# Export function
+def reset_servo():
+    servo.angle = -20
