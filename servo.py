4eb1746f026cf17e631ef882de1e3beba5305be5
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-05-20 22:52:51 +0200

    modif angle to angle_test

--- servo.py
+++ servo.py
@@ -8,9 +8,9 @@ def map_value(x, in_min, in_max, out_min, out_max):
 servo = AngularServo(18, min_pulse_width=0.0003, max_pulse_width=0.0023)
 
 def control_servo(value):
-    angle = map_value(value, -1, 1, -90, 90)
-    servo.angle = angle
-    print(f"Servo angle: {angle}")
+    angle_test = map_value(value, -1, 1, -90, 90)
+    servo.angle = angle_test
+    print(f"Servo angle: {angle_test}")
 
 def reset_servo():
     servo.angle = 0
@@ -21,4 +21,4 @@ if __name__ == "__main__":
         while True:
             value = controller.get_left_joystick_value(joystick)
             control_servo(value)
-            sleep(0.1)
+            sleep(0.01)
