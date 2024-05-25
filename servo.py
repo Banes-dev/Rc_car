b22009eb32fb81cd8b3ac774287ab3433d95c0fd
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-05-25 02:54:14 +0200

    test to patch steer

--- servo.py
+++ servo.py
@@ -8,9 +8,19 @@ def map_value(x, in_min, in_max, out_min, out_max):
 servo = AngularServo(18, min_pulse_width=0.0003, max_pulse_width=0.0023)
 
 def control_servo(value):
-    angle_test = map_value(value, -1, 1, -90, 90)
+    # angle_test = map_value(value, -1, 1, -90, 90)
+    # servo.angle = angle_test
+    # print(f"Servo angle: {angle_test}")
+    servo.angle = 0
+    sleep(2)
     servo.angle = 90
-    print(f"Servo angle: {angle_test}")
+    sleep(2)
+    servo.angle = 0
+    sleep(2)
+    servo.angle = -90
+    sleep(2)
+    servo.angle = 0
+    sleep(2)
 
 def reset_servo():
     servo.angle = 0
