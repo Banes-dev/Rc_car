d9d4b1a87f8b0250e1b361ec9fe9dc3a3ad54916
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-09-26 01:45:50 +0200

    change the value for pwm

--- c++/Servo.cpp
+++ c++/Servo.cpp
@@ -44,12 +44,18 @@ void Servo::MoveServo(int angle)
 		std::cout << "The angle of servo must be between 0 and 180" << std::endl;
 		return ;
 	}
-    // La position du servo est définie par un signal PWM entre 500µs et 2500µs
-    // En général, les servos ont un angle de 0 à 180 degrés, ce qui correspond à une durée de 500 à 2500 microsecondes
-    int pulseWidth = (500 + (angle * 2000) / 180); // Convertit l'angle en durée d'impulsion
+    // // La position du servo est définie par un signal PWM entre 500µs et 2500µs
+    // // En général, les servos ont un angle de 0 à 180 degrés, ce qui correspond à une durée de 500 à 2500 microsecondes
+    // int pulseWidth = (500 + (angle * 2000) / 180); // Convertit l'angle en durée d'impulsion
 
-    // Envoyer le signal PWM
-    pwmWrite(this->_gpio_pin, pulseWidth);
+    // // Envoyer le signal PWM
+    // pwmWrite(this->_gpio_pin, pulseWidth);
+
+
+	int pulseWidth = 300 + (angle * 1900) / 180;  // 300µs (min) + la portion de 1900µs en fonction de l'angle
+    int pwmValue = pulseWidth * 2000 / 2200;      // Conversion de la largeur d'impulsion en valeur PWM (0-2000)
+
+    pwmWrite(this->_gpio_pin, pwmValue);          // Écrire le signal PWM
 	std::cout << "Fin move servo" << std::endl;
 	return ;
 }
