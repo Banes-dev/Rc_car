2a8bcc03c47945c095b2e67c6ca89d1c75a7d004
Banes <86240550+Banes-dev@users.noreply.github.com>
2024-09-27 01:12:31 +0200

    test value

--- c++/main.cpp
+++ c++/main.cpp
@@ -12,13 +12,13 @@ int main(void)
     while(1)
     {
         std::cout << "← Gauche" << std::endl;
-        servo.MoveServo(300);
+        servo.MoveServo(401);
         std::this_thread::sleep_for(std::chrono::seconds(2));
         std::cout << "↓ Milieux ↓" << std::endl;
         servo.MoveServo(137);
         std::this_thread::sleep_for(std::chrono::seconds(2));
         std::cout << "→ Droite" << std::endl;
-        servo.MoveServo(10);
+        servo.MoveServo(1);
         std::this_thread::sleep_for(std::chrono::seconds(2));
     }
     return (0);
