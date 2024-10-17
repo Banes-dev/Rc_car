00dc27135a025668c2b866103f5e879dcc8e405e
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-10-17 03:11:50 +0200

    remove map in main.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -5,10 +5,6 @@
 #include "Servo.hpp"
 #include "Controller.hpp"
 
-int map(int x, int in_min, int in_max, int out_min, int out_max)
-{
-    return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
-}
 
 int main(int argc, char **argv)
 {
