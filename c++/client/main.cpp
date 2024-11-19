ea77e498150b76e2a760b2f79bd674368b730a9d
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-11-19 22:01:37 +0100

    argc and argv use in client good this time

--- c++/client/main.cpp
+++ c++/client/main.cpp
@@ -7,8 +7,8 @@
 
 int main(int argc, char **argv)
 {
-    void(argc);
-    void(argv);
+    (void)argc;
+    (void)argv;
     nRF24L01 module;
 
     std::cout << Blue << " Client started " << Green << "✔" << std::endl;
