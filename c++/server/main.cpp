4ccfd455ff2858a698553779f72c6b7ca4d61805
Banes <86240550+Banes-v2@users.noreply.github.com>
2024-11-24 14:48:06 +0100

    add timer on main server

--- c++/server/main.cpp
+++ c++/server/main.cpp
@@ -49,6 +49,7 @@ int main(int argc, char **argv)
         {
             module.ReceiveCommand(angle, power);
             std::cout << "Angle : " << angle << " Power : " << power << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 6ms de delai
         }
         // while(1)
         // {
